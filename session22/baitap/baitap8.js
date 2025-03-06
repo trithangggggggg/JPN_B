ddea5a25d9611c2cd483acccb8651cf5cb92be71
trithangggggggg <trithang2006vn@gmail.com>
2025-03-07 00:02:28 +0700

    add

--- session22/baitap/baitap8.js
+++ session22/baitap/baitap8.js
@@ -1,1 +1,28 @@
-let numbers = [3, 1, 4, 3, 2, 4, 4, 2, 3, 2];
+let array = prompt("Moi ban hap mot mang so nguyen gonm 10 phan tu ");
+let counts = {};
+let arr = array.split("");
+
+for (let i = 0; i < arr.length; i++) {
+    let num = arr[i];
+    if (counts[num] === undefined) {
+        counts[num] = 1;
+    } else {
+        counts[num]++;
+    }
+}
+let maxCount = 0;
+let minValue = Infinity;
+
+for (let key in counts) {
+    let value = parseInt(key);
+    let count = counts[key];
+
+    if (count > maxCount) {
+        maxCount = count;
+        minValue = value;
+    } else if (count === maxCount && value < minValue) {
+        minValue = value;
+    }
+}
+
+console.log(`Phần tử xuất hiện nhiều nhất là: ${minValue}`);
